d07c7b8c39462ab7765c5442b0bd35d1aae5b350
DZhang3210 <53838139+DZhang3210@users.noreply.github.com>
2022-06-01 17:30:10 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,8 @@ export default function Home() {
           ))}
         </div>
       </div>
-      <div className="mt-40 mob:mt-2 laptop:mt-40 mob:p-2 laptop:p-0">
+
+      {/*<div className="mt-40 mob:mt-2 laptop:mt-40 mob:p-2 laptop:p-0">
         <h1 className="text-2xl text-bold">Services.</h1>
         <div className="mt-10 grid grid-cols-2 mob:grid-cols-1 laptop:grid-cols-2 gap-6">
           {data.services.map((service, index) => (
@@ -70,7 +71,8 @@ export default function Home() {
             />
           ))}
         </div>
-      </div>
+          </div>*/}
+
       <div
         className="mt-40 mob:mt-2 laptop:mt-40 mob:p-2 laptop:p-0"
         ref={aboutRef}>
